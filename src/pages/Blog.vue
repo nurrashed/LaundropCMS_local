f61179a61eb33f18aff856a5d7df73546dd459ef
Nur Mohammad Rashed <rashed_huawei@yahoo.com>
2020-05-19 00:47:23 +0200

    footer

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -1,13 +1,13 @@
 <template>
   <Layout>
-    <div class="container mx-auto grid grid-cols-2 gap-2 pt-8">
+    <div class="container mx-auto grid w-full gap-2 pt-8">
       <div v-for="blog in $static.allBlog.edges" :key="blog.id" class="w-full shadow border border-gray-100 p-4 flex flex-col justify-between">
         <div class="h-full">
           <h4 class="text-2xl font-bold text'-gray-800">{{ blog.node.title }}</h4>
           <g-image :src="blog.node.thumbnail" class="pt-4"></g-image>
           <p>{{ blog.node.intro }}</p>
         </div>
-        <g-link class="mt-4 mb-4 block text-center py-2 rounded px-4 bg-primary text-white hover:bg-white hover:text-primary" :to="blog.node.path">SE MER</g-link>
+        <g-link class="mt-4 block text-center py-2 rounded px-4 bg-primary text-white hover:bg-white hover:text-primary" :to="blog.node.path">SE MER</g-link>
       </div>
     </div>
   </Layout>
